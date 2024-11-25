ba1ca9cc5766a792e6d8e5bce65f60205763566f
Quentin Castel <quentin.castel@dataiku.com>
2024-11-25 17:20:46 +0100

    Remove messaging

--- platform-ui/src/app/app.config.ts
+++ platform-ui/src/app/app.config.ts
@@ -4,7 +4,8 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideAnimations(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), NG_EVENT_PLUGINS]
+  providers: [provideAnimations(), provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), NG_EVENT_PLUGINS, provideAnimationsAsync()]
 };
