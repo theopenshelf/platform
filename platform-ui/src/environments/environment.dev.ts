9914d09adea77d0820c89638547ce75583ad94df
Quentin Castel <quentin.castel@dataiku.com>
2025-03-05 17:08:52 +0100

    Run local UI in dev mode against the backend

--- platform-ui/src/environments/environment.dev.ts
+++ platform-ui/src/environments/environment.dev.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  API_BASE_PATH: 'http://localhost:8486',
-  useMockApi: true,
-  demoMode: true,
+  API_BASE_PATH: 'http://localhost:8486/api',
+  useMockApi: false,
+  demoMode: false,
   defaultLocale: 'fr',
 };
