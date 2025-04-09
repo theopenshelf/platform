62367ec8d4a2b9aacdc1ec67b52a456f03f03ea9
Quentin Castel <quentin.castel@dataiku.com>
2025-04-09 22:29:56 +0200

    fix routes

--- platform-ui/src/app/portal/hub/pages/libraries/library.routes.ts
+++ platform-ui/src/app/portal/hub/pages/libraries/library.routes.ts
@@ -6,24 +6,29 @@ import { LibraryBorrowRecordsComponent } from "./library-borrow-records/library-
 import { LibraryItemsComponent } from "./library-items/library-items.component";
 import { LibraryComponent } from "./library/library.component";
 
-
-export const libraryRoutes: Routes = [{
-    path: '',
-    providers: [...hubProviders, ...getGlobalProviders()],
-    component: LibraryComponent,
-    children: [
-        { path: '', redirectTo: 'items', pathMatch: 'full' },
-        {
-            path: 'approval',
-            component: LibraryApprovalComponent
-        },
-        {
-            path: 'borrow-records',
-            component: LibraryBorrowRecordsComponent
-        },
-        {
-            path: 'items',
-            component: LibraryItemsComponent
-        }
-    ]
-}]; 
+export const libraryRoutes: Routes = [
+    {
+        path: '',
+        providers: [...hubProviders, ...getGlobalProviders()],
+        component: LibraryComponent,
+        children: [
+            {
+                path: '',
+                redirectTo: 'items',
+                pathMatch: 'full'
+            },
+            {
+                path: 'items',
+                component: LibraryItemsComponent
+            },
+            {
+                path: 'approval',
+                component: LibraryApprovalComponent
+            },
+            {
+                path: 'borrow-records',
+                component: LibraryBorrowRecordsComponent
+            }
+        ]
+    }
+]; 
